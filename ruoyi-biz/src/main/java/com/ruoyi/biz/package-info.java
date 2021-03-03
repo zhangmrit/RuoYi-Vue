5669ce102c490b8cc0689b4688d8c00a1d414964
zhangmrit <zhangmrit@126.com>
2021-03-03 12:44:05 +0800

    添加业务包空包

---
+++ ruoyi-biz/src/main/java/com/ruoyi/biz/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * @(#)package-info.java 2021年3月3日 下午12:30:46
+ * Copyright 2021 zmr, Inc. All rights reserved. 
+ * PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+/**
+ * <p>File：package-info.java</p>
+ * <p>Title: 业务包声明</p>
+ * <p>Description:</p>
+ * <p>Copyright: Copyright (c) 2021 2021年3月3日 下午12:30:46</p>
+ * <p>Company: zmrit.com </p>
+ * @author zmr
+ * @version 1.0
+ */
+package com.ruoyi.biz;
