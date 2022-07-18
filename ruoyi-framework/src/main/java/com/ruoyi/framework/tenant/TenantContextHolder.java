1687b304ef12108d0a0fbd3aaa46a1046be430b6
wind <zhangmrit@126.com>
2022-07-18 17:22:59 +0800

    引入TTL来传递线程池之间的变量

--- ruoyi-framework/src/main/java/com/ruoyi/framework/tenant/TenantContextHolder.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/tenant/TenantContextHolder.java
@@ -1,14 +1,19 @@
 package com.ruoyi.framework.tenant;
 
+import com.alibaba.ttl.TransmittableThreadLocal;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 租户holder
  */
+@Slf4j
 public class TenantContextHolder
 {
     /**
      * 支持父子线程之间的数据传递
      */
-    private static final ThreadLocal<String> CONTEXT = new ThreadLocal<>();
+    private static final ThreadLocal<String> CONTEXT = new TransmittableThreadLocal<>();
 
     public static void setTenantId(String tenant)
     {
