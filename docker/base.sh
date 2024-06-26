867f4e03d48ee5d0224dd51998dbfc767f239f37
zhangmrit <zhangmrit@126.com>
2024-06-26 19:41:40 +0800

    docker配置优化

---
+++ docker/base.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker-compose up -d ry-mysql ry-redis
--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,60 +1,13 @@
 #!/bin/sh
 
-# 使用说明，用来提示输入参数
-usage() {
-	echo "Usage: sh 执行脚本.sh [port|base|start|stop|rm]"
-	exit 1
-}
+cd ../
+git pull
+mvn clean package -DskipTests
 
-# 开启所需端口
-port(){
-	firewall-cmd --add-port=1024/tcp --permanent
-	firewall-cmd --add-port=3000/tcp --permanent
-	firewall-cmd --add-port=3306/tcp --permanent
-	firewall-cmd --add-port=6379/tcp --permanent
-	firewall-cmd --add-port=8000/tcp --permanent
-	firewall-cmd --add-port=8012/tcp --permanent
-	firewall-cmd --add-port=8080/tcp --permanent
-	service firewalld restart
-}
+cd ruoyi-ui
+npm install
+npm run build
 
-# 启动基础环境（必须）
-base(){
-	docker-compose up -d ry-mysql ry-redis
-}
-
-start(){
-	docker-compose up -d ry-server ry-ui
-}
-
-# 关闭所有环境/模块
-stop(){
-	docker-compose stop
-}
-
-# 删除所有环境/模块
-rm(){
-	docker-compose rm
-}
-
-# 根据输入参数，选择执行对应方法，不输入则执行使用说明
-case "$1" in
-"port")
-	port
-;;
-"base")
-	base
-;;
-"start")
-	start
-;;
-"stop")
-	stop
-;;
-"rm")
-	rm
-;;
-*)
-	usage
-;;
-esac
+cd ../docker
+sh copy.sh
+docker-compose up -d -V --build --no-deps ry-server ry-ui
